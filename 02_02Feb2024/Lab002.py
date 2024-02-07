20d346701a763fc6c24627517297ab3cb2318de4
Raghudr <raghunandandr@gmail.com>
2024-02-07 08:40:59 +0530

    name

---
+++ 02_02Feb2024/Lab002.py
@@ -0,0 +1,17 @@
+#Task #1 - Take a user input (name, age, roll_number, phone_number) and print the user details
+#Task #2 - Print the Table of 2 by using the command print()
+#2 x 1 = 2
+#2 x 2 = 4
+#...
+#2 x 10 = 20
+
+
+
+name = input ("Enter your name : ")
+age = input ("Enter your age : ")
+roll_number = input ("Enter your roll_number : ")
+phone_number = input ("Enter your phone_number : ")
+print ("name : ",name)
+print ("age : ",age)
+print("roll_number  : ",roll_number)
+print ("phone_number : ",phone_number)
