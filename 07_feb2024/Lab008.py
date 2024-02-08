1e5b7e653cb3be992001b77ba61d40a96b02fc78
Raghudr <raghunandandr@gmail.com>
2024-02-09 01:57:18 +0530

    name

---
+++ 07_feb2024/Lab008.py
@@ -0,0 +1,16 @@
+#Task #1
+
+# 1.Explain the difference between the = operator and the == operator in Python.
+''''when the values of both the operands are equal. Thus, the condition would become true is == operator
+The ‘==’ is known as the equality operator. comparison operator,x=5,y=5,x==y true
+The ‘=’ is known as the identity operator.   Assignment operator ,x=5
+Assign the value of the right side of the expression to the left side operand '''
+
+# 2.What does the ** operator do in Python, and how is it used?
+
+'''the ** operator is used for exponentiation. It raises the left operand to the power of the right operand.
+   result=2**3
+   result=8'''
+
+# 3. What does the ^ operator do in Python, and in what context is it commonly used
+''' ^ operator is the bitwise XOR (exclusive OR) operator. It performs a bitwise XOR operation between two integers.'''
